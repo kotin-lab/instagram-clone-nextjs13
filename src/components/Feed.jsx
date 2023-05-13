5beebfb0f583a871128eb7b8133ebde774311d5c
Kotin Lab <tinhtunaungcu.cu@gmail.com>
2023-05-13 23:39:42 +0630

    Make the feed section responsive

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,15 +6,15 @@ import Posts from './Posts';
 
 export default function Feed() {
   return (
-    <div>
-      <section>
+    <div className='grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto'>
+      <section className='md:col-span-2'>
         {/* Stories */}
         <Stories />
 
         {/* Posts */}
         <Posts />
       </section>
-      <section>
+      <section className='hidden md:block'>
         {/* Mini Profile */}
 
         {/* Suggestions */}
