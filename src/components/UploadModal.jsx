a4648800507a7a30ff461395ea5c321066dc3fd7
Kotin Lab <tinhtunaungcu.cu@gmail.com>
2023-05-15 22:16:17 +0630

    Install and implement react-modal

--- src/components/UploadModal.jsx
+++ src/components/UploadModal.jsx
@@ -2,17 +2,20 @@
 
 import { modalState } from "@/atom/modalAtom";
 import { useRecoilState } from "recoil";
+import Modal from 'react-modal';
 
 export default function UploadModal() {
   const [open, setOpen] = useRecoilState(modalState);
 
-
-  return (
-    <div>
-      <h1>Upload Modal</h1>
-      {open && (
-        <h1>Model open</h1>
-      )}
-    </div>
-  )
+  return open && (
+    <Modal
+      isOpen={open}
+      onRequestClose={() => setOpen(false)}
+      className={'max-w-lg w-[90%] h-[300px] absolute top-56 left-1/2 -translate-x-1/2 bg-white border-2 border-gray-300 rounded-md shadow-md outline-none'}
+    >
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1>Modal</h1>
+      </div>
+    </Modal>
+  );
 }
